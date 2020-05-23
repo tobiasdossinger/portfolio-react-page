6344a2e77fe3e8631da7ba3f456d0d40f467d143
Tobias Dossinger <tobias.dossinger@stud.h-da.de>
2020-05-24 01:14:26 +0200

    init

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import './App.css';
+import Particles from 'react-particles-js';
+import data from "./data";
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App" >
+        <Particles
+          params={data}
+        />
+        <Header title="🛠 This page is currently in development. 🛠" />
+      </div>
+    );
+  }
+}
+
+
+class Header extends Component {
+  render() {
+
+    return (
+      <header className="App-header">
+        <p>
+          {this.props.title}
+        </p>
+      </header>
+
+    )
+  }
+}
+export default App;
